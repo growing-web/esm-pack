20466618cbfb9aba7c1a117bd94139a468226ae3
vben <anncwb@126.com>
2022-06-14 10:35:57 +0800

    feat: add favicon handle

--- delivr-server/src/modules/api/api.controller.ts
+++ delivr-server/src/modules/api/api.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Res } from '@nestjs/common'
+import type { Response } from 'express'
 
 @Controller()
 export class ApiController {
   @Get('/health')
   async ping() {
     return 'health'
+  }
+
+  @Get('/favicon.ico')
+  async favicon(@Res() res: Response) {
+    res.status(204).send('204')
   }
 }
